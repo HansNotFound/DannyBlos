5193ae1963ec2f92eedb6f6fe1a3e945a54e2229
Rafael Christo Gracia <rafaelc.gracia@gmail.com>
2022-11-27 21:44:22 +0700

    Menu Screen

--- DannyBlos/Assets/Scripts/GameManager.cs
+++ DannyBlos/Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
     public GameObject rootCanvas;
     public GameObject gameOverScreen;
+    public GameObject menuScreen;
 
     public int world { get; private set; }
     public int stage { get; private set; }
@@ -34,11 +35,17 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         Application.targetFrameRate = 60;
-        NewGame();
+        MenuScreen();
+    }
+
+    public void MenuScreen() 
+    {
+        menuScreen.gameObject.SetActive(true);
     }
 
     public void NewGame()
     {
+        menuScreen.gameObject.SetActive(false);
         gameOverScreen.gameObject.SetActive(false);
 
         lives = 3;
