09f5d758d503f1d427d7e246363f68cd8e8399e3
Nealson William <nealsonwilliam@gmail.com>
2022-12-11 18:03:13 +0700

    Movement
    Perbaikan Koopa
    Menambah script movement easy

---
+++ DannyBlos/Assets/Scripts/GoombaEasy.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class GoombaEasy : MonoBehaviour
+{
+    public Sprite flatSprite;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Player player = collision.gameObject.GetComponent<Player>();
+
+            if (player.starpower) {
+                Hit();
+            } else if (collision.transform.DotTest(transform, Vector2.down)) {
+		        AudioManager.PlaySound(AudioManager.main.goomba, 1);
+                Flatten();
+            } else {
+                player.Hit();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Shell")) {
+            Hit();
+        }
+    }
+
+    public void Flatten()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<EntityMovementEasy>().enabled = false;
+        GetComponent<AnimatedSprite>().enabled = false;
+        GetComponent<SpriteRenderer>().sprite = flatSprite;
+        Destroy(gameObject, 0.5f);
+    }
+
+    public void Hit()
+    {
+        GetComponent<AnimatedSprite>().enabled = false;
+        GetComponent<DeathAnimation>().enabled = true;
+        Destroy(gameObject, 3f);
+    }
+
+}
